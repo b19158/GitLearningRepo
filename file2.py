9d50531740db6f65c34bf380cbbd87f45ca0c44b
Deepak <b19158@students.iitmandi.ac.in>
2020-11-28 21:50:46 +0530

    file 2 added

---
+++ file2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 18:37:16 2020
+
+@author: LENOVO
+"""
+
+
+#print("Hello World")
+print("Hello Again")
+print("I don't like typing")
+print("Yay Printing")
+print("I'd much rather you 'not'")
+print("I said do not touch this")
